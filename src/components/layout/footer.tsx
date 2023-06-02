07dcff9520eec10ef7f8476fb49cb54fa01c8d71
Marlon <marlon.deocampo@shipteksolutions.com>
2023-06-02 22:56:26 +0800

    first commit

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <div className="fixed bottom-0 w-full flex justify-center items-center h-14 px-10 bg-primary text-white">
+            <div>All Rights Reserved </div>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/products.tsx
@@ -0,0 +1,7 @@
+const Products: React.FC = () => {
+    return (
+        <div>this is products</div>
+    );
+};
+
+export default Products;
